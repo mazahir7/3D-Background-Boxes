2e1837cc3a7ea8f9f58e6940dc9bbbef8e7f1f96
Mazahir Attari <91468649+mazahir7@users.noreply.github.com>
2024-01-03 17:18:14 +0530

    files uploaded

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const magicBtn = document.getElementById("btn");
+const boxesContainer = document.getElementById("boxes");
+
+magicBtn.addEventListener("click", function () {
+  boxesContainer.classList.toggle("big");
+});
+
+function createBoxes() {
+
+  for (let i = 0; i < 4; i++) {
+
+    for (let j = 0; j < 4; j++) {
+      const box = document.createElement("div");
+      box.classList.add("box");
+      box.style.backgroundPosition = `-${j * 125}px -${i * 125}px`;
+
+      boxesContainer.appendChild(box);
+    }
+  }
+}
+createBoxes();
